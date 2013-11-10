5455bb7e7a8b7bdbfdd2233c155b33b6655eaefb
Hardmath123 <hardmath123@gmail.com>
2013-11-09 19:47:04 -0800

    oh noes timeout

--- static/query.js
+++ static/query.js
@@ -10,6 +10,7 @@ window.addEventListener("load", function() {
 				document.getElementById("search-result").innerHTML = xhr.responseText;
 			} else {
 				document.getElementById("search-result").innerHTML = "<h3>Oh noes.</h3>We don't have a Human entry for "+ document.getElementById("search-term").value +" yet!";
+				setTimeout(function(){document.getElementById("search-result").innerHTML = ""},10000);
 			}
 		};
 		xhr.send();
